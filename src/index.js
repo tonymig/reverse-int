07cc3b2bf9ccb6691dacb7e9dfccd36de371637c
tonymig <60812114+tonymig@users.noreply.github.com>
2020-03-21 21:24:35 +0300

    init: completed reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let toStringSplit = n.toString().split('');
+    if (toStringSplit[0] !== '-') {
+        return Number(toStringSplit.reverse().join(''));
+    }
+    else {
+        let exceptOfMinus = '';
+        for (let i = 1; i < toStringSplit.length; i++) {
+            exceptOfMinus += toStringSplit[i];
+        }
+        return Number(exceptOfMinus.split('').reverse().join(''));
+    }
 }
